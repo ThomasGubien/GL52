e90a52760718adbd27a3006e1e32fb15f2e38e3d
Taranti <pierre.neau86@gmail.com>
2019-04-30 13:42:38 +0200

    test

--- Projet/app/routes/qcm.js
+++ Projet/app/routes/qcm.js
@@ -7,6 +7,7 @@ const _ = require('lodash')
 const  URL= 'mongodb://devgl52:kkgfFbXM6XR0d2tk@database-shard-00-00-kldkd.mongodb.net:27017,database-shard-00-01-kldkd.mongodb.net:27017,database-shard-00-02-kldkd.mongodb.net:27017/test?ssl=true&replicaSet=Database-shard-0&authSource=admin&retryWrites=true'
 
 router.get('/',  (req, res, next) =>{
+    console.log('QCMID '+req.query.qcmID)
     res.render('createQCM', {
         title: 'createQCM'
     })
