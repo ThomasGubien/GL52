aee60ea4e48760d5a377d73b7571f5f34a2726dd
Taranti <pierre.neau86@gmail.com>
2019-05-07 00:25:32 +0200

    Ca fonctionne enfin

--- Projet/app/app.js
+++ Projet/app/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const fileUpload = require('express-fileupload');
+
 
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
@@ -12,7 +14,7 @@ const qcmRouter=require('./routes/qcm')
 const fileRouter=require('./routes/upload')
 
 var app = express();
-
+app.use(fileUpload())
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
