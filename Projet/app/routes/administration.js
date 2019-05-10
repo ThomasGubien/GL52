df377774273833a53f48a2077f5c1f51d76da36a
ThomasGubien <48588050+ThomasGubien@users.noreply.github.com>
2019-05-10 18:45:37 +0200

    add users gestion

--- Projet/app/routes/administration.js
+++ Projet/app/routes/administration.js
@@ -1,11 +1,31 @@
-var express = require('express');
-var router = express.Router();
+'use strict'
+const express = require('express')
+const router = express.Router()
+const MongoClient = require('mongodb')
+const _ = require('lodash')
+
+
+const URL = 'mongodb://devgl52:kkgfFbXM6XR0d2tk@database-shard-00-00-kldkd.mongodb.net:27017,database-shard-00-01-kldkd.mongodb.net:27017,database-shard-00-02-kldkd.mongodb.net:27017/test?ssl=true&replicaSet=Database-shard-0&authSource=admin&retryWrites=true'
+
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', async (req, res, next) => {
+    const client = await MongoClient.connect(
+        URL,
+        { useNewUrlParser: true }
+    )
+    const db = client.db('gl52')
+    const collection = db.collection('users')
+    const arr = await collection.find().toArray()
+    const usersarr = arr.map((qcm, index) => {
+        return qcm
+    })
+    client.close()
+    console.log(usersarr)
     res.render('administration', {
         chemin: 'Settings',
-        title: 'Administration'
+        title: 'Administration',
+        users: usersarr
     });
 });
 
--- Projet/app/routes/users.js
+++ Projet/app/routes/users.js
@@ -1,11 +1,31 @@
-var express = require('express');
-var router = express.Router();
+'use strict'
+const express = require('express')
+const router = express.Router()
+const MongoClient = require('mongodb')
+const _ = require('lodash')
+
+
+const URL = 'mongodb://devgl52:kkgfFbXM6XR0d2tk@database-shard-00-00-kldkd.mongodb.net:27017,database-shard-00-01-kldkd.mongodb.net:27017,database-shard-00-02-kldkd.mongodb.net:27017/test?ssl=true&replicaSet=Database-shard-0&authSource=admin&retryWrites=true'
+
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+router.get('/', async (req, res, next) => {
+    const client = await MongoClient.connect(
+        URL,
+        { useNewUrlParser: true }
+    )
+    const db = client.db('gl52')
+    const collection = db.collection('users')
+    const userinfo = await collection.findOne({name:'GUBIEN'})
+    /*const userinfo = arr.map((qcm, index) => {
+        return qcm
+    })*/
+    client.close()
+    console.log(userinfo)
     res.render('userProfile', {
         chemin: 'Settings',
-        title: 'Profil'
+        title: 'Profil',
+        profil: userinfo
     });
 });
 
