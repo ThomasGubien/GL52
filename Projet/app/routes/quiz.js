014058b6798dc0b38f5d64dfc46142346e53cc1f
EloiseGrillet <grilleteloise@gmail.com>
2019-06-13 19:56:23 +0200

    do not merge : wip result creation
    do not merge :
    need same logic than answers to save answers

--- Projet/app/routes/quiz.js
+++ Projet/app/routes/quiz.js
@@ -288,5 +288,48 @@ router.post('/new', async (req, res)=>{
     res.redirect('/quiz/manage')
 })
 
+router.get('/result/:quiz_id', async (req, res, next) => {
+    console.log('QCMID ' + req.params.quiz_id)
+    const client = await MongoClient.connect(
+        URL,
+        { useNewUrlParser: true }
+    )
+    const db = client.db('gl52')
+    const collection = db.collection('questionnaires')
+    const q = await collection.findOne({ title: req.params.quiz_id })
+    const collection2 = db.collection('answers')
+    const userAnswers = await collection2.findOne({ title: req.params.quiz_id })
+    client.close()
+    console.log(q)
+    console.log(userAnswers)
+    res.render('result', {
+        chemin: 'Quiz',
+        title: 'Answer',
+        answers: userAnswers.answers,
+        quiz: q,
+        questions: q.questions
+    })
+})
+
+router.get('/list', async (req, res, next) => {
+    console.log('QCMID ' + req.query.qcmID)
+    const client = await MongoClient.connect(
+        URL,
+        { useNewUrlParser: true }
+    )
+    const db = client.db('gl52')
+    const collection = db.collection('questionnaires')
+    const arr = await collection.find().toArray()
+    const quizarr = arr.map((qcm, index) => {
+        return qcm
+    })
+    client.close()
+    console.log(quizarr)
+    res.render('viewResults', {
+        chemin: 'Quiz',
+        title: 'Answer',
+        quiz: quizarr
+    })
+})
 
 module.exports = router
